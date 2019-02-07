92e384e5b413b7423a4c24f324db5ff2418e50f2
boeydaryl <boeydaryl@gmail.com>
2019-02-07 10:53:12 +0100

    Comments on script for user friendliness

--- Scripts/sample_PCA.R
+++ Scripts/sample_PCA.R
@@ -1,10 +1,10 @@
-#PCA of samples
+#### Script to generate histograms, MA plots, and Dispersion Estimates from res and dds ####
 
 hist (res$padj, breaks=20, col="grey")
 plotMA(res, ylim = c(-1, 1))
 plotDispEsts(dds)
 
-
+#PCA of samples
 rld <- vst(dds)
 ramp <- 1:3/3
 cols <- c(rgb(ramp, 0, 0),
