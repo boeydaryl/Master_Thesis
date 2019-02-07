4beb6bc9be727364b9497b46c18397c1309aa42c
boeydaryl <boeydaryl@gmail.com>
2019-02-07 17:17:29 +0100

    minor revisions

--- Scripts/GSA_KEGG.R
+++ Scripts/GSA_KEGG.R
@@ -6,22 +6,21 @@ library(piano)
 
 
 ##### To prepare geneLevelStats file from DESeq Res #####
-"res <- res[ ! (is.na(res$pvalue) | is.na(res$padj)), ]
+res <- res[ ! (is.na(res$pvalue) | is.na(res$padj)), ]
 geneLevelStats <- as.data.frame(res[,c("log2FoldChange","padj")])
 mart <- useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host="www.ensembl.org")
 ensembl2name <- getBM(attributes=c("ensembl_gene_id","external_gene_name"),mart=mart)
 geneLevelStats <- merge(x=geneLevelStats, y=ensembl2name, by.x=0, by.y=1, all.x=TRUE)
 colnames(geneLevelStats) <- c("ensembl","log2fc","padj","gene")
 rownames(geneLevelStats) <- geneLevelStats[,1]
-geneLevelStats <- geneLevelStats[order(geneLevelStats$padj),]"
-##### To prepare geneLevelStats file #####
-
+geneLevelStats <- geneLevelStats[order(geneLevelStats$padj),]
 
 #### Continue from here if geneLevelStats file exists #####
 geneLevelStats <- read.csv("/Volumes/scRNAseq_1/P5_vs_IM90/Analysis/p5vsIM90_geneLevelStat.csv")
 
 #### To load appropriate Gene set under gsc #######
-gsc <- loadGSC("/Volumes/scRNAseq_1/Gene_Sets/KEGG/c2.cp.kegg.v6.2.symbols.gmt.txt", type="gmt")
+#gsc <- loadGSC("/Volumes/scRNAseq_1/Gene_Sets/KEGG/c2.cp.kegg.v6.2.symbols.gmt.txt", type="gmt")
+gsc <- loadGSC("/Volumes/scRNAseq_1/Gene_Sets/GSEA/Hallmark_sets/h.all.v6.2.symbols.gmt.txt", type="gmt")
 padj <- geneLevelStats$padj
 log2fc <- geneLevelStats$log2fc
 names(padj) <- names(log2fc) <- geneLevelStats$gene
@@ -40,9 +39,9 @@ GSAheatmap(gsaRes, cutoff = 5, adjusted = FALSE, ncharLabel = 25,
            colorgrad = NULL, cex = NULL)
 
 
-#For GSA Summary Table and Boxplot #####
-#GSAsummaryTable(gsaRes, save=T, file="/Volumes/scRNAseq_1/P4_vs_IM90/Analysis/p4_vs_im90gsares.txt")
-geneSetSummary(gsaRes, "HALLMARK_APOPTOSIS")
+##### For GSA Summary Table and Boxplot #####
+#GSAsummaryTable(gsaRes, save=T, file="/Volumes/scRNAseq_1/P5_vs_IM90/Analysis/p5_vs_im90gsares_KEGG.txt")
+geneSetSummary(gsaRes, "HALLMARK_GLYCOLYSIS")
 boxplot(list(-log10(geneLevelStats$padj),
              -log10(geneSetSummary(gsaRes,"HALLMARK_APOPTOSIS")$geneLevelStats)),
         names=c("all","HALLMARK_APOPTOSIS"))
