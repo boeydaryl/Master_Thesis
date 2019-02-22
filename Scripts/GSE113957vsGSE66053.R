00353dfb2920eafedcca574f7d4ab4180a9529a2
boeydaryl <boeydaryl@gmail.com>
2019-02-22 10:43:29 +0100

    new tximport scripts for new runs

---
+++ Scripts/GSE113957vsGSE66053.R
@@ -0,0 +1,32 @@
+# Script to import salmon transcript abundance files through DESeq analysis
+
+###### Necessary libraries to run script #######
+library(DESeq2)
+library(readr)
+library(tximport)
+
+#### Directory for abundance files ######
+dir1 <- "/Volumes/scRNAseq_1/DESeq runs/GSE113957vsGSE66053/"
+
+#### To load sample metadata and search for related salmon files #####
+samples <- read.csv(file.path(dir1, "GSE113957vsGSE66053.csv"), header = TRUE)
+files <- file.path(dir1, samples$Well.number,"quant.sf")
+
+#### Annotation from transcripts to genes #####
+txdb <- makeTxDbFromGFF("/Volumes/scRNAseq_1/Homo_sapiens/ENSEMBL/Homo_sapiens.GRCh38.95.gtf")
+k <- keys(txdb, keytype ="TXNAME")
+tx2gene <- select(txdb, k, "GENEID", "TXNAME")
+names(files) <- paste0(samples$Well.number, "_sample", 1:81)
+all(file.exists(files))
+
+#### Actual importation step ####
+txi <- tximport(files, type = "salmon", tx2gene = tx2gene, ignoreTxVersion = TRUE)
+ddsTxi <- DESeqDataSetFromTximport(txi,colData = samples, design = ~ Sample)
+
+#### Runs DESeq and saves results as res ######
+dds <- DESeq(ddsTxi)
+res <- results(dds)
+res
+
+write.csv(res, file = 
+            "/Volumes/scRNAseq_1/DESeq runs/GSE113957vsGSE66053/Analysis/GSE113957vsGSE66053_deseq.csv")
---
+++ Scripts/p1p2p3vsGSE113957.R
@@ -0,0 +1,32 @@
+# Script to import salmon transcript abundance files through DESeq analysis
+
+###### Necessary libraries to run script #######
+library(DESeq2)
+library(readr)
+library(tximport)
+
+#### Directory for abundance files ######
+dir1 <- "/Volumes/scRNAseq_1/DESeq runs/p1p2p3_vs_GSE113957/"
+
+#### To load sample metadata and search for related salmon files #####
+samples <- read.csv(file.path(dir1, "p1p2p3vsGSE113957.csv"), header = TRUE)
+files <- file.path(dir1, samples$Well.number,"quant.sf")
+
+#### Annotation from transcripts to genes #####
+txdb <- makeTxDbFromGFF("/Volumes/scRNAseq_1/Homo_sapiens/ENSEMBL/Homo_sapiens.GRCh38.95.gtf")
+k <- keys(txdb, keytype ="TXNAME")
+tx2gene <- select(txdb, k, "GENEID", "TXNAME")
+names(files) <- paste0(samples$Well.number, "_sample", 1:148)
+all(file.exists(files))
+
+#### Actual importation step ####
+txi <- tximport(files, type = "salmon", tx2gene = tx2gene, ignoreTxVersion = TRUE)
+ddsTxi <- DESeqDataSetFromTximport(txi,colData = samples, design = ~ Condition)
+
+#### Runs DESeq and saves results as res ######
+dds <- DESeq(ddsTxi)
+res <- results(dds)
+res
+
+write.csv(res, file = 
+            "/Volumes/scRNAseq_1/DESeq runs/p1p2p3_vs_GSE113957/Analysis/p1p2p3vsGSE113957_deseq.csv")
